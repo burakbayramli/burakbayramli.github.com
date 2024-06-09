580f3b22d63ced9ff090ab597f18d5ed6bbc1aeb
BB <me@yomama.com>
2024-06-09 14:07:10 +0300

    -

---
+++ main.js
@@ -0,0 +1,30 @@
+
+function init() {
+    var rt = localStorage.getItem('reminder_text');
+    const date = new Date();
+    const d1 = date.getDate();
+    const m1 = date.getMonth() + 1;
+    const y1 = date.getFullYear();    
+    console.log(rt);
+    console.log(d1, m1, y1);
+    var s = "";
+    if (rt != null) {
+	rt = JSON.parse(rt);
+	Object.keys(rt).forEach(function (x) {
+	    console.log('------------------------');
+	    console.log(x, rt[x]);
+	    var [d2,m2,y2] = x.split("/");
+	    console.log(d2,m2,y2);
+	    rtext = rt[x];
+	    if ( (d1==d2 | d2=="*") & (m1==m2|m2=="*") & (y1==y2 | y2=="*") ) {
+		console.log("match");
+		s += `<p><font color="red">- ${rtext}</font></p>`;
+	    }
+	});
+    }
+    if (s.length > 0) {
+	s += '<font size="1.5px"><a href="/static/reminders/index.html">Detail</a></font>';
+	document.getElementById('reminder').innerHTML = s;
+    }
+}
+
