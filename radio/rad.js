1a1616dc96ebb74e155ec4a44b79143304eddf7f
BB <me@yomama.com>
2024-07-26 16:11:13 +0300

    -

--- radio/rad.js
+++ radio/rad.js
@@ -1,13 +1,4 @@
 
-var stations = [
-    'https://somafm.com/groovesalad32.pls',
-    'https://somafm.com/secretagent32.pls',
-    'http://209.236.126.18:8004/listen.pls',
-    'https://laradiofm.com/download/394-en-pls',
-    'http://199.233.234.34:25373/listen.pls',
-    'https://somafm.com/covers32.pls',
-    'http://kcrw.streamguys1.com/kcrw_192k_mp3_news_internet_radio'];
-
 var urls = {
     'SomaFM: Groove Salad':'https://ice4.somafm.com/groovesalad-32-aac',
     'SomaFM: Secret Agent':'https://ice2.somafm.com/secretagent-32-aac',
@@ -15,7 +6,7 @@ var urls = {
     'Radio Paradise':'https://stream-tx3.radioparadise.com/mp3-32',
     'Gold Instrumental':'http://199.233.234.34:25373/stream',
     'SomaFM: Covers':'https://ice4.somafm.com/covers-32-aac',
-    'KCRW':'http://kcrw.streamguys1.com/kcrw_192k_mp3_news_internet_radio'
+    'OPB':'http://flume.opb.org/radio_lbr.mp3#MP3#National'
 };
 
 function init() {
