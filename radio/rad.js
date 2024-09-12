d9e2fe8aeba1db03541c321af1f1012011b4ef56
BB <me@yomama.com>
2024-09-12 13:52:54 +0300

    -

--- radio/rad.js
+++ radio/rad.js
@@ -5,7 +5,7 @@ var urls = {
     '1Mix Radio':'http://fr2.1mix.co.uk:8060/32aac',
     'Radio Paradise':'https://stream-tx3.radioparadise.com/mp3-32',
     'SomaFM: Covers':'https://ice4.somafm.com/covers-32-aac',
-    'WVIA': 'https://playerservices.streamtheworld.com/pls/WVIAFM.pls'
+    'WVIA': 'https://26223.live.streamtheworld.com:443/WVIAFM_SC'
 };
 
 function init() {
