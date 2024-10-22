41fcbc2bc4bc9b690ea00eea400aae4aa4683886
BB <me@yomama.com>
2024-10-22 13:57:03 +0300

    -

--- calc/scripts/InvalidResultError.js
+++
@@ -1,7 +0,0 @@
-export default class InvalidResultError extends Error {
-  constructor(message = '', ...args) {
-    super(message, ...args);
-    this.message = `The result is ${message}`;
-    this.name = 'InvalidResultError';
-  }
-}
