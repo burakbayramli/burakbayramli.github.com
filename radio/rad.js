3b250313abab978ee30fcfafc3ec2e474c152245
BB <me@yomama.com>
2024-11-14 15:29:13 +0300

    -

--- radio/rad.js
+++ radio/rad.js
@@ -5,9 +5,9 @@ var urls = {
     '1Mix Radio':'http://fr2.1mix.co.uk:8060/32aac',
     'Radio Paradise':'https://stream-tx3.radioparadise.com/mp3-32',
     'SomaFM: Covers':'https://ice4.somafm.com/covers-32-aac',
-    "Lush": "https://somafm.com/lush32.pls",
-    "Beat Blender": "https://somafm.com/beatblender32.pls",
-    "GOA": "https://somafm.com/suburbsofgoa32.pls",
+    "Lush": "https://ice2.somafm.com/lush-32-aac",
+    "Beat Blender": "https://ice4.somafm.com/beatblender-32-aac",
+    "GOA": "http://ice4.somafm.com/suburbsofgoa-32-aac",
     'WVIA': 'https://26223.live.streamtheworld.com:443/WVIAFM_SC'
 }    
 
