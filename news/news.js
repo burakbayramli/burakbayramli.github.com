a2a1368cb04c365f453ca7502f89285aa8b72612
BB <me@yomama.com>
2025-05-16 09:18:01 +0300

    -

--- news/news.js
+++ news/news.js
@@ -17,15 +17,15 @@ function saveShowHideTextBox(){
 }
 
 var news_sources = [["Politico","https://www.politico.com/rss/politicopicks.xml"],
-		    ["TDB","https://api.allorigins.win/raw?url=https://www.thedailybeast.com/arc/outboundfeeds/rss/articles"],
-		    ["The Guardian","https://api.allorigins.win/raw?url=http://www.theguardian.com/world/rss"],
+//		    ["TDB","https://api.allorigins.win/raw?url=https://www.thedailybeast.com/arc/outboundfeeds/rss/articles"],
+//		    ["The Guardian","https://api.allorigins.win/raw?url=http://www.theguardian.com/world/rss"],
 		    ["CNBC","https://www.cnbc.com/id/100727362/device/rss/rss.html"],
 		    ["NYT", "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"],
-		    ["France 24","https://api.allorigins.win/raw?url=https://www.france24.com/en/rss"],
-		    ["Al Monitor", "https://api.allorigins.win/raw?url=https://www.al-monitor.com/rss"],
-		    ["TASS", "https://api.allorigins.win/raw?url=http://tass.com/rss/v2.xml"],
+//		    ["France 24","https://api.allorigins.win/raw?url=https://www.france24.com/en/rss"],
+//		    ["Al Monitor", "https://api.allorigins.win/raw?url=https://www.al-monitor.com/rss"],
+//		    ["TASS", "https://api.allorigins.win/raw?url=http://tass.com/rss/v2.xml"],
 		    ["First Post","https://www.firstpost.com/commonfeeds/v1/mfp/rss/world.xml"],
-		    ["WION","https://api.allorigins.win/raw?url=https://www.wionews.com/feeds/world/rss.xml"],
+//		    ["WION","https://api.allorigins.win/raw?url=https://www.wionews.com/feeds/world/rss.xml"],
 		    ["Arab News","https://www.arabnews.com/cat/3/rss.xml"]
 		   ];
 		    
