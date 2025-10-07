f256a07406799b5aeaf40322bf3083f97a6cdd28
BB <me@yomama.com>
2025-10-07 13:01:04 +0300

    -

--- news/news.js
+++ news/news.js
@@ -24,9 +24,8 @@ var news_sources = [["Politico","https://www.politico.com/rss/politicopicks.xml"
 //		    ["France 24","https://api.allorigins.win/raw?url=https://www.france24.com/en/rss"],
 //		    ["Al Monitor", "https://api.allorigins.win/raw?url=https://www.al-monitor.com/rss"],
 //		    ["TASS", "https://api.allorigins.win/raw?url=http://tass.com/rss/v2.xml"],
-		    ["First Post","https://www.firstpost.com/commonfeeds/v1/mfp/rss/world.xml"],
 //		    ["WION","https://api.allorigins.win/raw?url=https://www.wionews.com/feeds/world/rss.xml"],
-		    ["Arab News","https://www.arabnews.com/cat/3/rss.xml"]
+		    ["First Post","https://www.firstpost.com/commonfeeds/v1/mfp/rss/world.xml"]
 		   ];
 		    
 function get_news() {
